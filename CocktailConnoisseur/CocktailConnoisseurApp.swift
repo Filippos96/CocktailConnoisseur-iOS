486440ebc0f04a2a416ef9c29e23a2bb00c4737a
Filip Karlsson <filip_karlsson96@hotmail.com>
2024-03-06 14:30:21 +0100

    Initial Commit

---
+++ CocktailConnoisseur/CocktailConnoisseurApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailConnoisseurApp.swift
+//  CocktailConnoisseur
+//
+//  Created by Jasmin on 2024-03-06.
+//
+
+import SwiftUI
+
+@main
+struct CocktailConnoisseurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
